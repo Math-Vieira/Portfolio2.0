0b2b232b2c446256e8a517257cfcfd4bad38610e
math-vieira <matheusvieira9153@gmail.com>
2023-02-07 11:37:20 -0300

    refactor: refactor useMediaQuery

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,44 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 function useMediaQuery(query: string): boolean {
-	const getMatches = (query: string): boolean => {
-		// Prevents SSR issues
-		if (typeof window !== "undefined") {
-			return window.matchMedia(query).matches;
-		}
-		return false;
-	};
+  const [matches, setMatches] = useState<boolean>(() =>
+    typeof window !== 'undefined' ? window.matchMedia(query).matches : false
+  )
 
-	const [matches, setMatches] = useState<boolean>(getMatches(query));
+  useEffect(() => {
+    const media = window.matchMedia(query)
+    if (media.matches !== matches) {
+      setMatches(media.matches)
+    }
+    const listener = (): void => {
+      setMatches(media.matches)
+    }
+    media.addEventListener('change', listener)
+    return () => {
+      media.removeEventListener('change', listener)
+    }
+  }, [query, matches])
 
-	function handleChange() {
-		setMatches(getMatches(query));
-	}
-
-	useEffect(() => {
-		const matchMedia = window.matchMedia(query);
-
-		// Triggered at the first client-side load and if query changes
-		handleChange();
-
-		// Listen matchMedia
-		if (matchMedia.addListener) {
-			matchMedia.addListener(handleChange);
-		} else {
-			matchMedia.addEventListener("change", handleChange);
-		}
-
-		return () => {
-			if (matchMedia.removeListener) {
-				matchMedia.removeListener(handleChange);
-			} else {
-				matchMedia.removeEventListener("change", handleChange);
-			}
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [query]);
-
-	return matches;
+  return matches
 }
 
-export default useMediaQuery;
+export default useMediaQuery
