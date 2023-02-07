69941020983ac6cfccbebcaa93a114bca77bfac6
math-vieira <matheusvieira9153@gmail.com>
2023-02-07 11:49:19 -0300

    feat: implement useMediaQuery and MyInfoContainer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,11 +6,13 @@ import SocialMediaBox from "../../components/SocialBox";
 import { ReactComponent as Linkedin } from "../../assets/svg/linkedin.svg";
 import { ReactComponent as GitHub } from "../../assets/svg/github.svg";
 import { ReactComponent as Instagram } from "../../assets/svg/instagram.svg";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 const Home = () => {
     const [isInitial, setIsInitial] = useState<boolean>(true);
     const ref = useRef<HTMLParagraphElement>(null);
     const occupation: string = "Desenvolvedor Web";
+    const isMobile = useMediaQuery('(max-width: 900px)');
 
     useEffect(() => {
         if (!isInitial && ref!.current!.innerHTML.length !== occupation.length) {
@@ -25,7 +27,7 @@ const Home = () => {
             <div className="centralizer">
                 <S.MainContentContainer>
                     <div className="nameLogoContainer">
-                        <div>
+                        <S.MyInfoContainer>
                             <S.Name>Matheus Vieira</S.Name>
                             <S.Occupation ref={ref}></S.Occupation>
                             <S.SocialMediaContainer>
@@ -39,8 +41,8 @@ const Home = () => {
                                     <Instagram />
                                 </SocialMediaBox>                               
                             </S.SocialMediaContainer>
-                        </div>
-                        <MainLogo />
+                        </S.MyInfoContainer>
+                        {!isMobile && <MainLogo />}
                     </div>
                 </S.MainContentContainer>
             </div>
