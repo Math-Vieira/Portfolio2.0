bcfdf64888a1610a5edbd9002cd212243b7536b4
math-vieira <matheusvieira9153@gmail.com>
2023-02-18 18:26:21 -0300

    feat: implement mobile navigation

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,37 +1,68 @@
-import React from "react";
+import useMediaQuery from "../../hooks/useMediaQuery";
+import { useToggle } from "../../hooks/useToggle";
 import * as S from "./style";
 
 const Header = () => {
-	return (
-		<S.Header>
-			<S.HeaderContentContainer className="centralizer">
-				<S.Logo>
-					<a href="/">
-						<span>/*</span>M.V DEV<span>*/</span>
-					</a>
-				</S.Logo>
-				<S.HeaderNav>
-					<ul>
-						<li>
-							<S.Link href="#start">Início</S.Link>
-						</li>
-						<li>
-							<S.Link href="#aboutMe">Sobre mim</S.Link>
-						</li>
-						<li>
-							<S.Link href="#projects">Projetos</S.Link>
-						</li>
-						<li>
-							<S.Link href="#courses">Cursos</S.Link>
-						</li>
-						<li>
-							<S.Link href="#skills">Habilidades</S.Link>
-						</li>
-					</ul>
-				</S.HeaderNav>
-			</S.HeaderContentContainer>
-		</S.Header>
-	);
+    const isMobile = useMediaQuery("(max-width: 700px)");
+    const { state: mobileNav, toggle: toggleMobileNav } = useToggle();
+    return (
+        <S.Header>
+            <S.HeaderContentContainer className="centralizer">
+                <S.Logo>
+                    <a href="/">
+                        <span>/*</span>M.V DEV<span>*/</span>
+                    </a>
+                </S.Logo>
+                <S.HeaderNav mobileNav={mobileNav}>
+                    <ul>
+                        <li>
+                            <S.Link href="#start">
+                                {isMobile && <i className="fas fa-house"></i>}
+                                {!isMobile && "Início"}
+                            </S.Link>
+                        </li>
+                        <li>
+                            <S.Link href="#aboutMe">
+                                {isMobile && (
+                                    <i className="fa-solid fa-book"></i>
+                                )}
+                                {!isMobile && "Sobre mim"}
+                            </S.Link>
+                        </li>
+                        <li>
+                            <S.Link href="#projects">
+                                {isMobile && (
+                                    <i className="fa-regular fa-pen-to-square"></i>
+                                )}
+                                {!isMobile && "Projetos"}
+                            </S.Link>
+                        </li>
+                        <li>
+                            <S.Link href="#courses">
+                                {isMobile && (
+                                    <i className="fa-solid fa-graduation-cap"></i>
+                                )}
+                                {!isMobile && "Cursos"}
+                            </S.Link>
+                        </li>
+                        <li>
+                            <S.Link href="#skills">
+                                {isMobile && (
+                                    <i className="fa-solid fa-list"></i>
+                                )}
+                                {!isMobile && "Habilidades"}
+                            </S.Link>
+                        </li>
+                    </ul>
+                </S.HeaderNav>
+                {isMobile && (
+                    <S.MobileMenu active={mobileNav} onClick={toggleMobileNav}>
+                        <i className="fa-solid fa-bars"></i>
+                    </S.MobileMenu>
+                )}
+            </S.HeaderContentContainer>
+        </S.Header>
+    );
 };
 
 export default Header;
--- src/layouts/Header/style.ts
+++ src/layouts/Header/style.ts
@@ -1,67 +1,103 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-	background-color: ${(props) => props.theme.primaryColor};
-	box-shadow: ${({ theme }) => theme.headerBoxShadow};
-	position: fixed;
-	width: 100%;
-	padding: 0.5rem;
-	top: 0;
-	z-index: 1;
+    background-color: ${props => props.theme.primaryColor};
+    box-shadow: ${({ theme }) => theme.headerBoxShadow};
+    position: fixed;
+    width: 100%;
+    padding: 0.5rem;
+    top: 0;
+    z-index: 1;
 `;
 
 export const HeaderContentContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
 `;
 
 export const Logo = styled.div`
-	font-size: 2rem;
-	cursor: pointer;
-	margin-top: 0.2rem;
-	font-weight: lighter;
-	& a {
-		color: ${({ theme }) => theme.textColor};
-	}
+    font-size: 2rem;
+    cursor: pointer;
+    margin-top: 0.2rem;
+    font-weight: lighter;
+    & a {
+        color: ${({ theme }) => theme.textColor};
+    }
 
-	& span {
-		color: ${({ theme }) => theme.mainColor};
-	}
+    & span {
+        color: ${({ theme }) => theme.mainColor};
+    }
 `;
 
-export const HeaderNav = styled.nav`
-	& ul {
-		display: flex;
-		gap: 1.5rem;
-	}
+export const HeaderNav = styled.nav<{ mobileNav: boolean }>`
+    & ul {
+        display: flex;
+        gap: 1.5rem;
+    }
 
-	& a {
-		color: ${({ theme }) => theme.textColor};
-		font-size: 1rem;
-		font-weight: 500;
-	}
+    & a {
+        color: ${({ theme }) => theme.textColor};
+        font-size: 1rem;
+        font-weight: 500;
+    }
+
+    @media (max-width: 700px) {
+        position: fixed;
+        right: ${({ mobileNav }) => (mobileNav ? 0 : "-2.3rem")};
+        top: 50%;
+        transform: translateY(-50%);
+        backdrop-filter: blur(3px);
+        background: rgba(255, 255, 255, 0.08);
+        box-shadow: 0px 0px 29px 0px rgb(0 0 0 / 32%);
+        padding: 1rem;
+        & ul {
+            flex-direction: column;
+            align-items: center;
+        }
+        & a {
+            color: ${({ theme }) => theme.textColor};
+        }
+    }
 `;
 
 export const Link = styled.a`
-	position: relative;
-	display: block;
-	&::after {
-		content: "";
-		position: absolute;
-		width: 100%;
-		background-color: red;
-		transform: scaleX(0);
-		height: 1px;
-		bottom: -7px;
-		left: 0;
-		background: linear-gradient(270deg, ${({theme}) => theme.mainColor} 0%, #17161d 100%);
-		transform-origin: bottom right;
-		transition: transform 0.5s cubic-bezier(0.86, 0, 0.07, 1);
-	}
-	&:hover::after {
-		transform: scaleX(1);
-		transform-origin: bottom left;
-	}
+    position: relative;
+    display: block;
+    &::after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        background-color: red;
+        transform: scaleX(0);
+        height: 1px;
+        bottom: -7px;
+        left: 0;
+        background: linear-gradient(
+            270deg,
+            ${({ theme }) => theme.mainColor} 0%,
+            #17161d 100%
+        );
+        transform-origin: bottom right;
+        transition: transform 0.5s cubic-bezier(0.86, 0, 0.07, 1);
+    }
+    &:hover::after {
+        transform: scaleX(1);
+        transform-origin: bottom left;
+    }
+
+    @media (max-width: 700px) {
+        &::after {
+            display: none;
+        }
+    }
+`;
+
+export const MobileMenu = styled.div<{ active: boolean }>`
+    cursor: pointer;
+    font-size: 2rem;
+    color: ${({ active, theme }) =>
+        active ? theme.mainColor : theme.textColor};
+    transition: 0s;
 `;
