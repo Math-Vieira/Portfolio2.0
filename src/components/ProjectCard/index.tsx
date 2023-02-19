db1a210f94ae58d3033659f6a1528fe6bfec57a6
math-vieira <matheusvieira9153@gmail.com>
2023-02-19 11:59:45 -0300

    feat: add features to ProjectCard

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,8 +1,59 @@
-import React from "react";
+import { useState } from "react";
 import * as S from "./style";
 
-const ProjectCard = ({ projectName }: { projectName: string }) => {
-    return <S.ProjectContainer>{projectName}</S.ProjectContainer>;
+interface ProjectInterface {
+    projectName: string;
+    projectRepo: string;
+    projectUrl?: string;
+    projectType: string;
+    status: string;
+}
+
+const ProjectCard = (props: ProjectInterface) => {
+    const [infoVisible, setInfoVisible] = useState<boolean>(false);
+    const turnInfoVisibleFalse = () => {
+        setInfoVisible(false);
+    };
+    const turnInfoVisibleTrue = () => {
+        setInfoVisible(true);
+    };
+    return (
+        <S.ProjectContainer
+            onMouseOut={turnInfoVisibleFalse}
+            onMouseOver={turnInfoVisibleTrue}
+        >
+            <S.ProjectType>{props.projectType}</S.ProjectType>
+            <S.ProjectStatus>{props.status}</S.ProjectStatus>
+            {!infoVisible ? (
+                <S.ProjectName className="animeLeft">
+                    {props.projectName}
+                </S.ProjectName>
+            ) : (
+                <S.InfoContainer className="animeLeft">
+                    <li>
+                        <a
+                            href={props.projectRepo}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Repositório
+                        </a>
+                    </li>
+                    {props.projectUrl && (
+                        <li>
+                            <a
+                                href={props.projectUrl}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Aplicação no ar
+                            </a>
+                        </li>
+                    )}
+                </S.InfoContainer>
+            )}
+        </S.ProjectContainer>
+    );
 };
 
 export default ProjectCard;
--- src/components/ProjectCard/style.ts
+++ src/components/ProjectCard/style.ts
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export const ProjectContainer = styled.div`
+    position: relative;
     width: 25rem;
     height: 25rem;
-    margin-bottom: 4rem;
     background-color: ${({ theme }) => theme.primaryColor};
-
     position: relative;
     &:before,
     &:after {
@@ -23,6 +22,52 @@ export const ProjectContainer = styled.div`
     }
     &:nth-child(1):before,
     &:nth-child(1):after {
-        background: linear-gradient(235deg, #00AD6F, #00AD6F, #00AD6F);
+        background: linear-gradient(235deg, #00ad6f, #00ad6f, #00ad6f);
+    }
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const ProjectType = styled.div`
+    color: ${({ theme }) => theme.textColor};
+    font-size: 1.3rem;
+    position: absolute;
+    top: 0.5rem;
+    right: 0.7rem;
+`;
+
+export const ProjectStatus = styled.div`
+    color: ${({ theme }) => theme.textColor};
+    font-size: 1.3rem;
+    position: absolute;
+    bottom: 0.5rem;
+    left: 0.7rem;
+`;
+
+export const ProjectName = styled.h3`
+    color: ${({ theme }) => theme.textColor};
+    font-size: 2rem;
+    letter-spacing: 2px;
+    text-align: center;
+    @media (max-width: 500px) {
+        font-size: 1.5rem;
+    }
+`;
+
+export const InfoContainer = styled.ul`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & li a {
+        color: ${({ theme }) => theme.textColor};
+        font-size: 1.5rem;
+    }
+
+    & li a:hover {
+        color: ${({ theme }) => theme.mainColor};
     }
 `;
