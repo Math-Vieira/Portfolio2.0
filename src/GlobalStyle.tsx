5e872476f02a6ebbf2f5e760a23ff850356f5127
math-vieira <matheusvieira9153@gmail.com>
2023-03-20 19:38:29 -0300

    feat: add smokeSwiper

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -8,6 +8,38 @@ const GlobalStyle = createGlobalStyle`
 .swiper-button-next {
     color:  ${props => props.theme.selectItemArrow};
 }
+.mySmokeSwiper {
+    position: relative;
+}
+
+.mySmokeSwiper::before {
+    content: '';
+    height: 20px;
+    width: 20px;
+    position: absolute;
+    background-color:${props => props.theme.primaryColor};
+    top: 0;
+    left: 0;
+    height: 100px;
+    height: 100%;
+    z-index: 2;
+    box-shadow: 0px 0px 20px 20px ${props => props.theme.primaryColor};
+}
+
+.mySmokeSwiper::after {
+    content: '';
+    height: 20px;
+    width: 20px;
+    position: absolute;
+    background-color: ${props => props.theme.primaryColor};
+    top: 0;
+    right: 0;
+    scroll-margin-right: 0;
+    height: 100px;
+    height: 100%;
+    z-index: 2;
+    box-shadow: 0px 0px 20px 20px ${props => props.theme.primaryColor};
+}
 `;
 
 export default GlobalStyle;
